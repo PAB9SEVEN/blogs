a8edae7728579d0bec22c5f809c1fa4a7a80b869
PAB9SEVEN <preetibangad97@gmail.com>
2017-11-20 21:36:03 +0530

    changed files after the addinion of mongolab

--- server.js
+++ server.js
@@ -1,30 +1,68 @@
 var express=require('express');
 var exphbs=require('express-handlebars');
 var path=require('path');
-var port=3000;
 var methodOverride=require('method-override');
-
+var passport=require('passport');
+var localStrategy=require('passport-local');
+var flash=require('connect-flash');
+var User=require('./models/user');
+var comment=require('./models/comment');
+var blog=require('./models/blog');
 var bodyParser=require('body-parser');
 var expressSanitizer=require('express-sanitizer');
 var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/blogapp');
+mongoose.connect('mongodb://blogpost:blogpost@ds131583.mlab.com:31583/blogsapp');
+//mongoose.connect('mongodb://localhost/blogapp');
 var app=express();
+var authroutes=require('./routes/user');
+var blogroutes=require('./routes/blog');
+var commentroutes=require('./routes/comment');
+var middleware=require('./middleware/new');
 app.set('views',path.join(__dirname,'views'));
 app.engine('handlebars',exphbs({defaultLayout:'layout'}));
 app.set('view engine','handlebars');
 app.set('partials',path.join(__dirname + '/views','partials'));
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
+app.use(flash());
+app.use(require('express-session')({
+    secret:"cats",
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(bodyParser.urlencoded({extended:false}));
 //tis should be always be aftre the body parser middleware
 app.use(expressSanitizer());
     app.use(express.static(path.join(__dirname,'public')));
     app.use('/static', express.static(__dirname + '/public'));
+app.use(function(req,res,next){
+    res.locals.currentuser=req.user;
+    res.locals.error=req.flash('error');
+    res.locals.success=req.flash('success');
+    next();
+});
+app.use(authroutes);
+app.use(blogroutes);
+app.use(commentroutes);
+/*
 var blogSchema=new mongoose.Schema({
     title:String,
     image:String,
     body:String,
+   author:{
+       username:String,
+        id:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+            
+        }
+    },
     created:{type:Date,default:Date.now}
 });
 var blog=mongoose.model('blog',blogSchema);
@@ -34,8 +72,12 @@ blog.create({
     image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXYoF9-EZ4mqN1VahxeQZXAoLgRjF_YqasGJ7JtTxghY8g9ohB',
     body:"first test blog post"
 });
-*/
+
 //restful routes
+app.get('/',function(req,res){
+    res.render('landing');
+});
+
 app.get('/blogs',function(req,res){
   //  console.log('blogs');
     blog.find({},function(err,blogs){
@@ -51,7 +93,7 @@ app.get('/blogs',function(req,res){
     });
     
 });
-app.get('/blogs/new',function(req,res){
+app.get('/blogs/new',middleware.isloggedin,function(req,res){
     res.render('new');
     
 });
@@ -59,13 +101,18 @@ app.post('/blogs',function(req,res){
     var title=req.body.title;
     var image=req.body.image;
     var body=req.body.body;
-    var newb={title:title,image:image,body:body}   
+    var author={
+        id:req.user._id,
+        name:req.user.username
+    }
+    var newb={title:title,image:image,body:body,author:author}   
    blog.create(newb,function(err,newblog){
        if(err){
            console.log(err);
            res.render('new');
                   }
 else{
+    console.log(newblog);
     console.log("Passed");
     res.redirect('/blogs');
 }       
@@ -78,7 +125,9 @@ app.get('/blogs/:id',function(req,res){
             res.render('/blogs');
         }
         else{
-            res.render('show',{blog:blogpost});
+            console.log(blogpost);
+            console.log(req.user);
+            res.render('show',{blog:blogpost,user:req.user});
         }
     });
     
@@ -126,7 +175,11 @@ res.redirect("/blogs");
     });
      
  });
-
-app.listen(port,function(){
-    console.log("server started");
+*/
+app.set('port',process.env.PORT||3000);
+app.listen(app.get('port'),function(){
+    console.log("server started on "+app.get('port'));
 });
+
+           
+           
